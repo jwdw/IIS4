78480667902f2e400d02e3d818bb64cad1eb9346
Jan Willem de Wit <jwdwit@gmail.com>
2016-10-26 13:44:59 +0200

    compute decision criterion

--- one.m
+++ one.m
@@ -18,7 +18,7 @@ maxEst=mle(S1);
 muS1=maxEst(1);
 sgS1=maxEst(2);
 xS1=linspace(muS1-4*sgS1,muS1+4*sgS1,200);
-pdfS1=1/sqrt(2*pi)/sgS1*exp(-(xS1-muS1).^2/(2*sgS1^2));
+pdfS1=1/(sqrt(2*pi)*sgS1)*exp(-(xS1-muS1).^2/(2*sgS1^2));
 plot(xS1,pdfS1,'b');
 axis([-80 100 0 0.04]);
 
@@ -28,7 +28,7 @@ maxEst=mle(S2);
 muS2=maxEst(1);
 sgS2=maxEst(2);
 xS2=linspace(muS2-4*sgS2,muS2+4*sgS2,200);
-pdfS2=1/sqrt(2*pi)/sgS2*exp(-(xS2-muS2).^2/(2*sgS2^2));
+pdfS2=1/(sqrt(2*pi)*sgS2)*exp(-(xS2-muS2).^2/(2*sgS2^2));
 plot(xS2,pdfS2,'r');
 
 plot(S1, zeros(length(S1)), 'bo');
@@ -48,8 +48,8 @@ plot(S2, zeros(length(S2)), 'ro');
 plot(T, zeros(length(T)), 'ks');
 
 syms x;
-solve(1/sqrt(2*pi)/sgS1*exp(-(x-muS1).^2/(2*sgS1^2))*P_1 == 1/sqrt(2*pi)/sgS2*exp(-(x-muS2).^2/(2*sgS2^2)) * P_2, x==0, x)
-
+a = solve(1/(sqrt(2*pi)*sgS1)*exp(-(x-muS1).^2/(2*sgS1^2))*P_1 == 1/(sqrt(2*pi)*sgS2)*exp(-(x-muS2).^2/(2*sgS2^2)) * P_2, x);
+eval(a)
 
 
 end
